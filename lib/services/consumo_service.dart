36afe42cb68f9fd490c013996ec9bd9048e1c567
Alex José Silva <alexjosesilvati@gmail.com>
2026-04-12 10:58:07 -0300

    dev | change url api of data ibge

--- lib/services/consumo_service.dart
+++ lib/services/consumo_service.dart
@@ -5,7 +5,7 @@ import '../models/consumo_regiao.dart';
 
 class ConsumoService {
   // Troque pela URL real do seu backend
-  static const String baseUrl = 'https://060eaa19-577a-48fd-a4be-0c27fb2ed453-00-21baq6ip3w4rg.picard.replit.dev:8000';
+  static const String baseUrl = 'https://b-2-bgoverno-apifast--alexjose.replit.app/';
 
   Future<List<ConsumoRegiao>> buscarConsumoPorRegiao({
     int ano = 2026,
